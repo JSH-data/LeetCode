9144106fc3f8e8c59b8a629e6e41f1818bf554a8
SeungHwa Jeong <62323657+JSH-data@users.noreply.github.com>
2022-03-07 22:37:55 +0900

    Time: 109 ms (18.79%), Space: 42.1 MB (26.63%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.js
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -4,14 +4,21 @@
  */
 var climbStairs = function(n) {
     const dp = new Array(n+1).fill(0)
-    dp[0] = 1
+//     dp[1] = 1
     
-    for(let i = 0; i <= n; i++) {
-        for(let j=0; j < i; j++) {
-            if(j + 1 == i || j + 2 == i) {
-                dp[i] += dp[j] 
-            }
-        }
+//     for(let i = 0; i <= n; i++) {
+//         for(let j=0; j < i; j++) {
+//             if(j + 1 == i || j + 2 == i) {
+//                 dp[i] += dp[j] 
+//             }
+//         }
+//     }
+    dp[1] = 1
+    dp[2] = 2
+    
+    for(let i=3; i<=n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2]
     }
+    
     return dp[n]
 };
